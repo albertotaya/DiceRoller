455b921ab81917947843376930bd3f03fe770cdb
Tadashi Yamamoto <albertotaya@gmail.com>
2020-03-17 16:01:49 -0300

    Dice Roller + Image

--- app/src/main/java/com/amazingthings/diceroller/MainActivity.kt
+++ app/src/main/java/com/amazingthings/diceroller/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.amazingthings.diceroller
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var currentDrawable = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,26 +27,46 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomInt = (1..6).random()
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        currentDrawable = (1..6).random()
+
+        val drawableResource = when (currentDrawable) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 
     private fun countResult() {
-        val countResultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
 
-        if ( countResultText.text == "Hello World!" )
-            countResultText.text = "1"
-        else if ( countResultText.text == "6" )
-            countResultText.text = "6"
-        else
-            countResultText.text = (countResultText.text.toString().toInt() + 1).toString()
+        if (currentDrawable == 0 )
+            currentDrawable = 1
+        else if ( currentDrawable < 6 )
+            currentDrawable++
+
+        val drawableResource = when (currentDrawable) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 
     private fun appReset() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "0"
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        currentDrawable = 1
+        diceImage.setImageResource(R.drawable.dice_1)
     }
 
 }
