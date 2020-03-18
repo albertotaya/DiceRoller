44687a2654bed15131817051695e0dd560002b9a
Tadashi Yamamoto <albertotaya@gmail.com>
2020-03-18 16:41:04 -0300

    Dice Roller + Image

--- app/src/main/java/com/amazingthings/diceroller/MainActivity.kt
+++ app/src/main/java/com/amazingthings/diceroller/MainActivity.kt
@@ -10,8 +10,10 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    var currentDrawable = 0
-    lateinit var diceImage : ImageView
+    var currentDrawable1 = 0
+    var currentDrawable2 = 0
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
     lateinit var rollButton : Button
     lateinit var countButton : Button
     lateinit var resetButton : Button
@@ -20,7 +22,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
         rollButton = findViewById(R.id.roll_button)
         countButton = findViewById(R.id.count_button)
         resetButton = findViewById(R.id.reset_button)
@@ -32,9 +35,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
 
-        currentDrawable = (1..6).random()
+        var drawableResource: Int
 
-        val drawableResource = when (currentDrawable) {
+        currentDrawable1 = (1..6).random()
+
+        drawableResource = when (currentDrawable1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -43,17 +48,32 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        diceImage.setImageResource(drawableResource)
+        diceImage1.setImageResource(drawableResource)
+
+        currentDrawable2 = (1..6).random()
+
+        drawableResource = when (currentDrawable2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage2.setImageResource(drawableResource)
     }
 
     private fun countResult() {
 
-        if (currentDrawable == 0 )
-            currentDrawable = 1
-        else if ( currentDrawable < 6 )
-            currentDrawable++
+        var drawableResource: Int
 
-        val drawableResource = when (currentDrawable) {
+        if (currentDrawable1 == 0 )
+            currentDrawable1 = 1
+        else if ( currentDrawable1 < 6 )
+            currentDrawable1++
+
+        drawableResource = when (currentDrawable1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -62,12 +82,30 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        diceImage.setImageResource(drawableResource)
+        diceImage1.setImageResource(drawableResource)
+
+        if (currentDrawable2 == 0 )
+            currentDrawable2 = 1
+        else if ( currentDrawable2 < 6 )
+            currentDrawable2++
+
+        drawableResource = when (currentDrawable2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage2.setImageResource(drawableResource)
     }
 
     private fun appReset() {
-        currentDrawable = 1
-        diceImage.setImageResource(R.drawable.dice_1)
+        currentDrawable1 = 1
+        diceImage1.setImageResource(R.drawable.dice_1)
+        currentDrawable2 = 1
+        diceImage2.setImageResource(R.drawable.dice_1)
     }
 
 }
